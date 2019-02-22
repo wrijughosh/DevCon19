ab48bc9742122307f3db3fc75346348da9fc1cc9
Wriju <wriju_ghosh@hotmail.com>
2019-02-22 20:48:57 +0530

    First Commit

---
+++ Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBWeb.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DBWeb.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class HRContext : DbContext
+    {
+        public HRContext(DbContextOptions<HRContext> options) : base(options)
+        {
+
+        }
+        public DbSet<DBWeb.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBWeb.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DBWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            string sConnectionString = "Server=sqlwgsvr.database.windows.net;Database=hrdb;User Id=bdotnet;Password=SuperSecret!;";
+
+            services.AddDbContext<HRContext>(options => options.UseSqlServer(sConnectionString));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
